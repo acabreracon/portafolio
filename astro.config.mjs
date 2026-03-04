4b9775f29aef716c2c960e609fb1a11fd61be880
Alejandro Cabrera <alexcabreracon@gmail.com>
2026-03-04 01:20:32 -0500

    fix server output with prerender

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
-
   devToolbar: {
-  enabled: false
-},
-
+    enabled: false
+  },
   integrations: [react()],
   adapter: vercel()
 });
